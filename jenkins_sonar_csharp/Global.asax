16d6eb9e644e17025216ee0d0f9b1c58adff5e14
Geovanny Ribeiro <geovannyribeiro@solsis.br>
2016-06-02 13:40:54 -0300

    initial commit

---
+++ jenkins_sonar_csharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jenkins_sonar_csharp.MvcApplication" Language="C#" %>
---
+++ jenkins_sonar_csharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jenkins_sonar_csharp.Startup))]
+namespace jenkins_sonar_csharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
