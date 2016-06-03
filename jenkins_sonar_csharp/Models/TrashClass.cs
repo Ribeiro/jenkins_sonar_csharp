492de0289237fc3412e970dd3cff6066202e6832
Geovanny Ribeiro <geovannyribeiro@solsis.br>
2016-06-03 09:35:31 -0300

    added TrashClass for Sonar check!

---
+++ jenkins_sonar_csharp/Models/TrashClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jenkins_sonar_csharp.Models
+{
+    public static class TrashClass
+    {
+
+        public static void trashItOnce()
+        {
+            Console.WriteLine("trashItOnce ...");
+        }
+
+
+        public static void trashItTwice()
+        {
+            Console.WriteLine("trashItTwice ...");
+        }
+
+    }
+
+}
